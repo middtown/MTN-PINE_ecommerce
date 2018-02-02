f185cb9733ca9593068052108f61f3a38654dd7f
joshrhughes <josh@mtnpine.us>
2018-02-02 10:48:30 -0700

    css to scss

--- front-end/src/app/cart/cart.component.ts
+++ front-end/src/app/cart/cart.component.ts
@@ -4,7 +4,7 @@ import { CARTCOUNT } from '../cart';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
 
