768a8389942d747494907c48881b709897fda9e3
rickytranmer <rwtranmer@gmail.com>
2018-02-04 09:14:24 -0700

    deploys latest build, serves mtnpine homepage on localhost

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -47,7 +47,7 @@ router.delete('/api/profile/address/delete', itemsController.deleteAddress);
 //to the front end if no back end routes exist (by serving up the Angular index.html file).
 // router.get('/*', itemsController.default);
 
-router.get('/*', (req, res) => {
+router.get('*', (req, res) => {
 	res.sendfile('index.html');
 });
 
