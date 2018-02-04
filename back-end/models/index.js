783bd40da1c075bf04267df9fbe68240ce94ff0d
rickytranmer <rwtranmer@gmail.com>
2018-02-04 08:41:29 -0700

    test route

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 
 // //connection to heroku and local comp for three people.
 let localHost = process.env.AARONSLOCALMACHINE || process.env.JOSHUASLOCALMACHINE || process.env.RICKYSLOCALMACHINE;
-let sequelize = new Sequelize(process.env.DATABASE_URL || localHost.aaron || localHost.josh || localHost.ricky );
+let sequelize = new Sequelize(process.env.DATABASE_URL || localHost.aaron || localHost.josh || 'postgres://rickytranmer@localhost:5432/tunr_relationships' );
 
 //Export models and Sequelize for seed and dbSetup
 module.exports.Sequelize = Sequelize;
