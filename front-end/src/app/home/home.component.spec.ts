77912aee0bc831ac6e9960ff86e25d57b497a7ee
rickytranmer <rwtranmer@gmail.com>
2018-02-05 15:58:47 -0700

    ready tests, require requisite components and services

--- front-end/src/app/home/home.component.spec.ts
+++ front-end/src/app/home/home.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SignUpComponent } from '../sign-up/sign-up.component';
+import { LogInComponent } from '../log-in/log-in.component';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +10,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ 
+        HomeComponent, 
+        SignUpComponent,
+        LogInComponent
+      ]
     })
     .compileComponents();
   }));
