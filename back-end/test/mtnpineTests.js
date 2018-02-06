868ff2fb09b4854218a83f81fee5bef1d54dd688
middtown <aarontm9@gmail.com>
2018-02-05 17:54:25 -0700

    testing

--- back-end/test/mtnpineTests.js
+++ back-end/test/mtnpineTests.js
@@ -1,13 +1,27 @@
-var assert = require('assert');
-describe('Array', function() {
-	describe('#indexOf()', function() {
-		it('should return -1 when the value is not present', function() {
-    		assert.equal([1,2,3].indexOf(4), -1);
-    	});
-    	it('double done', function(done) {
-  			// Calling `done()` twice is an error
-  			setImmediate(done);
-  			setImmediate(done);
-		});	
-  	});
+const Sequelize = require('sequelize');
+const db = require('../models'); //grabbing db from models folder for router controll
+const Item = db.models.Item; //specifies what exaclty the model needed
+const User = db.models.User;
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+// Connection to heroku and local comp for three people.
+const localHost = require("../env.js") || process.env.AARONSLOCALMACHINE || process.env.JOSHUASLOCALMACHINE || process.env.RICKYSLOCALMACHINE;
+let sequelize = new Sequelize(process.env.DATABASE_URL || localHost.aaron || localHost.josh || localHost.ricky );
+
+
+describe('#createUser()', () => {
+
+    it('should save without error', done => {
+        User.create({     
+          name: 'Hendricks',
+          email:'someemail@gmail.com',
+          password: 'somepassword'
+        }).then( newUser => {
+          expect(newUser.name).to.equal('Hendricks');
+          console.log(newUser);
+          done();
+        });
+    });
+
 });
+          
